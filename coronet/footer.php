a0940f742f71646e6547742d90c7f13a73826723
Jetey123 <152363198+Jetey123@users.noreply.github.com>
2023-11-29 13:15:12 +0800

    Add files via upload

---
+++ coronet/footer.php
@@ -0,0 +1,26 @@
+  <!-- Footer
+    ================================================== -->
+    <footer class="footer">
+      <div class="container">
+      <center>
+      
+      <font size="4">Coronet &COPY; <?= date('Y') ?> </font>
+      <hr />
+      
+      
+      <table border="0">
+     <tr>
+      <td align="center">
+       <p> Projected by: Cuarteron, Mena, Dimabayao, Rosell</p>
+      </td>
+      </tr>
+      
+
+  
+      </table>
+      
+      
+      </center>
+      
+      </div>
+    </footer>
